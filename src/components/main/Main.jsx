2459dd338f207aa2bc1406331e94b233c22bb91e
Solbon13 <solbon13@yandex.ru>
2021-04-09 16:19:04 +0800

    pagination

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,22 +1,51 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getRepos } from '../actions/repos'
 import Repo from './Repo/Repo'
+import './Main.less'
+import { setCurrentPage } from '../../reducers/reposReducer'
+import { createPages } from '../../utils/pagesCreator'
 
 const Main = () => {
-    const dispatch = useDispatch()
 
+    const dispatch = useDispatch()
     const repos = useSelector(state => state.repos.items)
+    const isFetching = useSelector(state => state.repos.isFetching)
+    const currentPage = useSelector(state => state.repos.currentPage)
+    const perPage = useSelector(state => state.repos.perPage)
+    const totalCount = useSelector(state => state.repos.totalCount)
+    const [searchValue, setSearchValue] = useState("")
+    const pagesCount = Math.ceil(totalCount/perPage)
+
+    const pages = []
+    createPages(pages, pagesCount, currentPage)
 
     useEffect(() => {
-        dispatch(getRepos())
-    }, [])
+        dispatch(getRepos(searchValue, currentPage, perPage))
+    }, [currentPage])
+
+    function searchHandle() {
+        dispatch(setCurrentPage(1))
+        dispatch(getRepos(searchValue, currentPage, perPage))
+    }
 
     return (
         <div>
-            {repos.map(repo =>
-                <Repo repo={repo}/>
-            )}
+            <div className="search">
+                <input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} type="text" placeholder='Input repo name' className="search-input" />
+                <button onClick={() => searchHandle()} className="search-btn">Search</button>
+            </div>
+            {
+                isFetching === false
+                    ? repos.map(repo => <Repo repo={repo} />)
+                    : <div className="fetching"></div>
+            }
+            <div className="pages">
+                {pages.map((page, index) => <span
+                key={index}
+                className={currentPage == page ? 'current-page' : 'page'}
+                onClick={() => dispatch(setCurrentPage(page))}>{page}</span>)}
+            </div>
         </div>
     )
 }
--- src/reducers/reposReducer.js
+++ src/reducers/reposReducer.js
@@ -1,7 +1,13 @@
 const SET_REPOS = 'SET_REPOS'
+const SET_IS_FETCHING = 'SET_IS_FETCHING'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+
 const defaultState = {
     items: [],
     isFetching: true,
+    currentPage: 1,
+    perPage: 10,
+    totalCount: 0
 }
 
 export default function reposReducer (state = defaultState, action) {
@@ -9,7 +15,19 @@ export default function reposReducer (state = defaultState, action) {
         case SET_REPOS:
             return {
                 ...state,
-                items: action.payload.items
+                items: action.payload.items,
+                totalCount: action.payload.total_count,
+                isFetching: false
+            }
+        case SET_IS_FETCHING:
+            return {
+                ...state,
+                isFetching: action.payload
+            }
+        case SET_CURRENT_PAGE:
+            return {
+                ...state,
+                currentPage: action.payload
             }
         default:
             return state
@@ -17,3 +35,5 @@ export default function reposReducer (state = defaultState, action) {
 }
 
 export const setRepos = (repos) => ({type: SET_REPOS, payload: repos})
+export const setIsFetching = (bool) => ({type: SET_IS_FETCHING, payload: bool})
+export const setCurrentPage = (currentPage) => ({type: SET_CURRENT_PAGE, payload: currentPage})
